73cf013cb88c13bb5b3f81ee07333e3376c42e21
Seila Gamo <seila.gamo@udarealestate.com>
2021-12-07 08:53:40 +0100

    07/2021

---
+++ 2021/07/python/aoc_2021_07_solution_part_2.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+with open(BASE_DIR / '../input.txt') as f:
+    # Load input
+    clean_line = f.read().splitlines()
+    positions = [int(internal_time) for internal_time in clean_line[0].split(',')]
+
+    min_fuel = 0
+    optimal_position = 0
+    sums = {}
+    for i, position in enumerate(positions):
+        fuel = 0
+        for j, other_position in enumerate(positions):
+            if i == j:
+                continue
+            difference = abs(position - other_position)
+            if difference in sums:
+                fuel += sums.get(difference)
+            else:
+                sums[difference] = sum(range(abs(position - other_position) + 1))
+                fuel += sums[difference]
+
+        if (min_fuel != 0 and fuel < min_fuel) or min_fuel == 0:
+            min_fuel = fuel
+            optimal_position = position
+
+    print(f'Result: {min_fuel}')
